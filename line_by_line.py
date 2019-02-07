86354f45d04f811446d3f2918056ced5972a91ee
Elsanussi Mneina <emneina@rrc.ca>
2019-02-06 20:47:32 -0600

    implemented non-blind blanks for lines: i.e. blanks that cover each word in a line with as many blanks as the characters in a word, instead of a single fixed-length blank covering the whole line

--- line_by_line.py
+++ line_by_line.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+import re
 
 from card_separation_tokens import convert_flashcards_to_csv_text
 
@@ -7,48 +8,51 @@ LINE_FOCUS_BLANK = '_____?_____'
 LINE_NON_FOCUS_BLANK = '___________'
 
 
-def print_text_to_line_by_line_flashcards_csv(text: str):
-    csv_text = convert_text_to_flashcards_as_csv(text)
+def print_text_to_line_by_line_flashcards_csv(text: str, blind_blank=True):
+    csv_text = convert_text_to_flashcards_as_csv(text, blind_blank=blind_blank)
     print(csv_text)
 
 
-def convert_text_to_flashcards_as_csv(text: str) -> str:
-    flashcards = convert_text_to_line_by_line_flashcards(text)
+def convert_text_to_flashcards_as_csv(text: str, blind_blank=True) -> str:
+    flashcards = convert_text_to_line_by_line_flashcards(text, blind_blank=blind_blank)
     csv_text = convert_flashcards_to_csv_text(flashcards)
     return csv_text
 
 
-def convert_text_to_line_by_line_flashcards(text: str) -> List[Tuple[str, str]]:
+def convert_text_to_line_by_line_flashcards(text: str, blind_blank=True) -> List[Tuple[str, str]]:
     text = text.replace('\n\n', NEW_DOUBLE_LINE_SYMBOL + '\n')
     lines = text.split('\n')
     i = 0
     results = []
 
     for _ in lines:
-        front_side, back_side = quiz_on_nth_line(i, lines)
+        front_side, back_side = quiz_on_nth_line(i, lines, blind_blank=blind_blank)
         results.append((front_side, back_side))
         i += 1
     return results
 
 
-def quiz_on_nth_line(n: int, lines: List[str], long_version=False) -> Tuple[str, str]:
+def quiz_on_nth_line(n: int, lines: List[str], long_version=False, blind_blank=True) -> Tuple[str, str]:
     i = 0
     front_side = ''
     back_side = ''
     for line in lines:
+        line = line.replace(NEW_DOUBLE_LINE_SYMBOL, '\n')
         if i == n - 1:
             front_side += line + '\n'
         elif i == n:
-            front_side += str(i + 1) + ') ' + LINE_FOCUS_BLANK
+            if blind_blank:
+                front_side += str(i + 1) + ') ' + LINE_FOCUS_BLANK
+            else:
+                front_side += str(i + 1) + ') ' + custom_blank_line_with_spaces_between_words_shown(line)
             back_side = line
         elif long_version:
             front_side += LINE_NON_FOCUS_BLANK + '\n'
         i += 1
     front_side = front_side.replace(NEW_DOUBLE_LINE_SYMBOL, '\n')
     back_side = back_side.replace(NEW_DOUBLE_LINE_SYMBOL, '\n')
-    return front_side, back_side
+    return front_side.rstrip(), back_side.rstrip()
 
 
 def custom_blank_line_with_spaces_between_words_shown(line: str):
-    import re
-    return re.sub(r'[a-zA-Z]', '_', line)
+    return re.sub(r'[a-zA-Z0-9]', '_', line)
--- test/test_line_by_line.py
+++ test/test_line_by_line.py
@@ -6,8 +6,13 @@ from line_by_line import convert_text_to_flashcards_as_csv, quiz_on_nth_line, \
 
 class TestLineByLine(unittest.TestCase):
     def test_convert_flashcards_to_csv_text__one_words(self):
-        result = convert_text_to_flashcards_as_csv('fox\ndove')
+        result = convert_text_to_flashcards_as_csv('fox\ndove', blind_blank=True)
         expected = '1) _____?_____$%$%fox*&*&fox\n2) _____?_____$%$%dove*&*&'
+        self.assertEqual(expected, result)
+
+    def test_convert_flashcards_to_csv_text__one_words__blanks_with_spaces(self):
+        result = convert_text_to_flashcards_as_csv('fox\ndove', blind_blank=False)
+        expected = '1) ___$%$%fox*&*&fox\n2) ____$%$%dove*&*&'
         self.assertEqual(expected, result)
 
     def test_quiz_on_nth_line___first_line(self):
@@ -23,4 +28,26 @@ class TestLineByLine(unittest.TestCase):
     def test_custom_blank_line_with_spaces_between_words_shown(self):
         result = custom_blank_line_with_spaces_between_words_shown('first second third')
         expected = '_____ ______ _____'
+        self.assertEqual(expected, result)
+
+    def test_custom_blank_line_with_spaces_between_words_shown__with_punctuation(self):
+        result = custom_blank_line_with_spaces_between_words_shown('first, second; third.')
+        expected = '_____, ______; _____.'
+        self.assertEqual(expected, result)
+
+    def test_custom_blank_line_with_spaces_between_words_shown__with_numerals(self):
+        result = custom_blank_line_with_spaces_between_words_shown('1, 2; third.')
+        expected = '_, _; _____.'
+        self.assertEqual(expected, result)
+
+    def test_quiz_on_nth_line___first_line__blanks_with_spaces(self):
+        result = quiz_on_nth_line(0, ['line1word1 line1word2', 'line2word1', 'line3word1'],
+                                  blind_blank=False)
+        expected = ('1) __________ __________', 'line1word1 line1word2')
+        self.assertEqual(expected, result)
+
+    def test_quiz_on_nth_line___not_first_line__blanks_with_spaces(self):
+        result = quiz_on_nth_line(1, ['line1word1 line1word2', 'line2word1', 'line3word1'],
+                                  blind_blank=False)
+        expected = ('line1word1 line1word2\n2) __________', 'line2word1')
         self.assertEqual(expected, result)
